998846994eee24c867022c8a55b5a60fc81c4719
Patolll <patryk_dmochowski@wp.pl>
2025-04-21 22:43:36 +0200

    hamburger nav

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+// Toggle mobile menu visibility when the hamburger icon is clicked
+
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  if (mobileMenu.classList.contains("max-h-0")) {
+    mobileMenu.classList.remove("max-h-0");
+    mobileMenu.classList.add("max-h-96");
+  } else {
+    mobileMenu.classList.add("max-h-0");
+    mobileMenu.classList.remove("max-h-96");
+  }
+});
